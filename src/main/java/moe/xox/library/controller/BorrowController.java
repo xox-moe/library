b5e456cd576c2fac08ccfdb750c3f2c0baeb9607
紫川 <xox.moe@outlook.com>
2019-04-24 16:32:43 +0800

    新增了借书和还书的服务

--- src/main/java/moe/xox/library/controller/BorrowController.java
+++ src/main/java/moe/xox/library/controller/BorrowController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("borrow")
 public class BorrowController extends BaseController {
 
     @Autowired
